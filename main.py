ad815a903bd443fbc3e88e4740d01bb616ffcb7a
OmmrGAZAR315 <omarahmedelgazzar600@gmail.com>
2024-09-25 23:03:28 +0300

    compatible with h5p_contents and h5p_contents_libraries

--- main.py
+++ main.py
@@ -2,16 +2,32 @@ import pandas as pd
 
 last_id = input("Enter the last id: ")
 time = input("Enter the time: ")
-file_path = input("Enter the file path: ")
+file_path = input("Enter the file path: ").strip('"')
 
-# Load CSV data into a pandas DataFrame
 data = pd.read_csv(file_path)
 
-# Start constructing the SQL query
 if time == "1":
-    query = "UPDATE h5p_contents\nSET library_id = CASE \n"
+    query = "UPDATE h5p_contents \nSET library_id = CASE\n"
 else:
-    query = "UPDATE h5p_contents_libraries\nSET library_id = CASE \n"
+    query = """
+ALTER TABLE h5p_contents_libraries 
+ADD version VARCHAR(3)
+DEFAULT 'old';
+"""
+    query += """
+ALTER TABLE `h5p_contents_libraries`
+DROP PRIMARY KEY,
+ADD PRIMARY KEY(
+    `content_id`,
+    `library_id`,
+    `dependency_type`,
+    `version`
+);
+"""
+    query += """
+UPDATE h5p_contents_libraries 
+SET library_id = CASE
+"""
 
 # Iterate over each row in the data to construct the CASE statements
 for _, row in data.iterrows():
@@ -24,11 +40,8 @@ query += f"END\nWHERE id IN ({old_ids})"
 query += f",\n and content_id <= {last_id}; \n"
 
 # Output the generated SQL query
-if time == "1":
-    with open(file_path + "1.txt", 'w') as file:
-        file.write(query)
-    print(f"Extracted text saved to: {file_path}.txt")
-else:
-    with open(file_path + "2.txt", 'w') as file:
-        file.write(query)
-    print(f"Extracted text saved to: {file_path}.txt")
+output_file = f"{file_path}{'1' if time == '1' else '2'}.txt"
+with open(output_file, 'w') as file:
+    file.write(query)
+
+print(f"Extracted text saved to: {output_file}")
