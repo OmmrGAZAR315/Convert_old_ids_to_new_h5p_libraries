515f0205dbb4c4ed0565c27cb5dfbc34518e82b7
OmmrGAZAR315 <omarahmedelgazzar600@gmail.com>
2024-09-25 22:39:59 +0300

    compatible with h5p_contents and h5p_contents_libraries

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import pandas as pd
 
 last_id = input("Enter the last id: ")
+time = input("Enter the time: ")
+file_path = input("Enter the file path: ")
+
 # Load CSV data into a pandas DataFrame
-file_path = 'C:\\Users\\omara\\Downloads\\mfisg.csv'  # Replace with the actual path to your CSV file
 data = pd.read_csv(file_path)
 
 # Start constructing the SQL query
-query = "UPDATE h5p_contents_libraries\nSET id = CASE \n"
+if time == "1":
+    query = "UPDATE h5p_contents\nSET library_id = CASE \n"
+else:
+    query = "UPDATE h5p_contents_libraries\nSET library_id = CASE \n"
 
 # Iterate over each row in the data to construct the CASE statements
 for _, row in data.iterrows():
@@ -16,9 +21,14 @@ for _, row in data.iterrows():
 old_ids = ', '.join(map(str, data['old_id'].unique()))
 query += f"END\nWHERE id IN ({old_ids})"
 
-query+= f",\n and content_id <= {last_id}; \n"
+query += f",\n and content_id <= {last_id}; \n"
 
 # Output the generated SQL query
-with open(file_path+".txt", 'w') as file:
-    file.write(query)
-print(f"Extracted text saved to: {file_path}.txt")
+if time == "1":
+    with open(file_path + "1.txt", 'w') as file:
+        file.write(query)
+    print(f"Extracted text saved to: {file_path}.txt")
+else:
+    with open(file_path + "2.txt", 'w') as file:
+        file.write(query)
+    print(f"Extracted text saved to: {file_path}.txt")
