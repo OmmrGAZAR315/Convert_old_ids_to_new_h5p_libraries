8707dcbe96de848bc932515a705238b16e222a0f
OmmrGAZAR315 <omarahmedelgazzar600@gmail.com>
2024-09-27 08:20:45 +0300

    last commit

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import pandas as pd
 
 last_id = input("Enter the last id: ")
-time = input("Enter the time: ")
+isItTable_h5p_content = input("Is it table h5p_content? (y/n): ")
 file_path = input("Enter the file path: ").strip('"')
 
 data = pd.read_csv(file_path)
 
-if time == "1":
+if isItTable_h5p_content == "y":
     query = "UPDATE h5p_contents \nSET library_id = CASE\n"
 else:
     query = """
@@ -36,21 +36,22 @@ for _, row in data.iterrows():
 # Close the CASE statement and add the WHERE clause to filter based on old IDs
 old_ids = ', '.join(map(str, data['old_id'].unique()))
 
-query += f"END\n" + (",version = 'new'" if time != "1" else "")
+query += f"END\n" + (",version = 'new'\n" if isItTable_h5p_content != "y" else "")
+
 query += f"WHERE library_id IN ({old_ids})\n"
 
-if time == "1":
-    query += f"AND id <= {last_id};\n"
-else:
+query += "AND " + ("id" if isItTable_h5p_content == "y" else f"content_id") + f" <= {last_id};\n"
+
+if isItTable_h5p_content != "y":
     query += f"""
 DELETE FROM h5p_contents_libraries
 WHERE version = 'old'
-AND content_id <= {last_id};
-    """
+AND content_id <= {last_id};\n
+"""
     query += "ALTER TABLE `h5p_contents_libraries` DROP `version`;"
 
 # Output the generated SQL query
-output_file = f"{file_path}{'1' if time == '1' else '2'}.txt"
+output_file = f"{file_path}{'h5p_contents' if isItTable_h5p_content == 'y' else 'h5p_contents_libraries'}.txt"
 with open(output_file, 'w') as file:
     file.write(query)
 
